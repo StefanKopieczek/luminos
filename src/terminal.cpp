5527cad32bb7cb3b60eebac1d4a1824099c5e58e
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-01-13 20:39:51 +0000

    Implement terminal scrolling

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -29,6 +29,21 @@ void terminal_initialize() {
     }
 }
 
+void terminal_scroll() {
+    for (size_t y = 1; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t from_idx = y * VGA_WIDTH + x;
+            const size_t to_idx = (y - 1) * VGA_WIDTH + x;
+            terminal_buffer[to_idx] = terminal_buffer[from_idx];
+        }
+    }
+
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+        const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
+        terminal_buffer[index] = vga_entry(' ', terminal_color);
+    }
+}
+
 void terminal_setcolor(uint8_t color) {
     terminal_color = color;
 }
@@ -41,6 +56,10 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 void terminal_newline() {
     terminal_row++;
     terminal_column = 0;
+    if (terminal_row >= VGA_HEIGHT) {
+        terminal_scroll();
+        terminal_row--;
+    }
 }
 
 void terminal_put_printable_char(char c) {
