87f14c3a17e02b3c699ead9aaa7a38bb498381b8
Stefan Kopieczek <stefankopieczek@gmail.com>
2020-04-30 00:29:06 +0100

    Set segment registers
    When the GDT is initialized we also need to set the segment registers.

--- src/load_gdt.s
+++ src/load_gdt.s
@@ -4,4 +4,13 @@
 load_gdt:
   mov 4(%esp), %edx
   lgdt (%edx)
+
+  mov $0x10, %ax
+  mov %ax, %ds
+  mov %ax, %es
+  mov %ax, %fs
+  mov %ax, %gs
+  mov %ax, %ss
+  jmp $0x08,$set_cs
+set_cs:
   ret
