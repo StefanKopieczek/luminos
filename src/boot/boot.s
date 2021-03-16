0584e0b2e369c4e578f6fb49ff3ea04850dd0b65
Stefan Kopieczek <stefankopieczek@gmail.com>
2021-03-16 22:38:36 +0000

    Higher Half WIP

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -13,7 +13,7 @@ kernel file, aligned at a 32-bit boundary.
 The signature is in its own section so the header can be forced to be
 within the first 8 KiB of the kernel file.
 */
-.section .multiboot
+.section .multiboot.data
 .align 4
 .long MAGIC
 .long FLAGS
@@ -32,63 +32,67 @@ standard, and de-facto extensions.
 The compiler will assume the stack is properly aligned, and failure to
 align the stack will result in undefined behaviour.
 */
-.section .bss
-.align 16
+.section .bootstrap_stack, "aw", @nobits
+    .align 16
 stack_bottom:
-.skip 16384  # 16 KiB
+    .skip 16384  # 16 KiB
 stack_top:
+
+/* Preallocate pages used for paging. */
+.section .bss, "aw", @nobits
+    .align 4096
+boot_page_directory:
+    .skip 4096
+boot_page_table1:
+    .skip 4096
 
 /* The linker script specifies _start as the entry point to the kernel,
 and the bootloader will jump to this position once the kernel has been
 loaded. It doesn't make sense to return from this function, as the
 bootloader is gone.
 */
-.section .text
+.section .multiboot.text
 .global _start
 .type _start, @function
 _start:
-    /*
-    The bootloader has loaded us into 32-bit protected mode on an x86
-    machine. Interrupts are disabled. Paging is disabled. The processor
-    state is as defined in the multiboot standard. The kernel has full
-    control of the CPU. The kernel can only make use of hardware
-    features and any code it provides as part of itself. There's no printf
-    function, unless the kernel provides its own <stdio.h> header and a
-    printf implementation. There are no security restrictions, no safeguards,
-    no debugging mechanisms - only what the kernel provides itself.
-    It has absolute and complete power over the machine.
-    */
+    movl $(boot_page_table1 - 0xC0000000), %edi
+    movl $0, %esi
+    movl $1023, %ecx
+1:
+    cmpl $_kernel_start, %esi
+    jl 2f
+    cmpl $(_kernel_end - 0xC0000000), %esi
+    jge 3f
+    movl %esi, %edx
+    orl $0x003, %edx
+    movl %edx, (%edi)
 
-    /*
-    To set up a stack, we set the esp register to point to the top of the
-    stack (as it grows downwards on x86 systems). This is necessarily done
-    in assembly, as languages such as C cannot function without a stack.
-    */
+2:
+    addl $4096, %esi
+    addl $4, %edi
+    loop 1b
+
+3:
+    movl $(0x000B8000 | 0x003), boot_page_table1 - 0xC0000000 + 1023 * 4
+    movl $(boot_page_table1 - 0xC0000000 + 0x003), boot_page_directory - 0xC0000000 + 0
+	movl $(boot_page_table1 - 0xC0000000 + 0x003), boot_page_directory - 0xC0000000 + 768 * 4
+    movl $(boot_page_directory - 0xC0000000), %ecx
+	movl %ecx, %cr3
+    movl %cr0, %ecx
+	orl $0x80010000, %ecx
+	movl %ecx, %cr0
+    lea 4f, %ecx
+	jmp *%ecx
+
+.section .text
+
+4:
+    movl $0, boot_page_directory + 0
+    movl %cr3, %ecx
+    movl %ecx, %cr3
     mov $stack_top, %esp
-
-    /*
-    This is a good place to initialize crucial processor state before the
-    high-level kernel is entered. It's best to minimize the early
-    environment where crucial features are offline. Note that the processor
-    is not fully initialized yet: Features such as floating point
-    instructions and instruction set extensions are not initialized yet.
-    The GDT should be loaded here. Paging should be enabled here.
-    C++ features such as global constructors and exceptions will require
-    runtime support to work as well
-    */
-
     call _init
-
-    /*
-    Enter the high-level kernel. The ABI requires the stack is 16-byte
-    aligned at the time of the call instruction (which afterwards pushes the
-    return pointer of size 4 bytes). The stack was originally 16-byte
-    aligned above, and we've since pushed a multiple of 16 bytes to the
-    stack (i.e. zero bytes), and the alignment is thus preserved and the call
-    is well defined.
-    */
     call kernel_main
-
     call _fini
 
     /*
@@ -110,5 +114,5 @@ _start:
 /*
 Set the size of the _start symbol to the current location '.' minus its start.
 This is useful when debugging or when you implement call tracing.
-*/
 .size _start, . - _start
+*/
